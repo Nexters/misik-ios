b125e3f188f96fe8d1d99bbac569a38e9b7ccfb8
haeseoklee <haeseoklee.dev@gmail.com>
2025-01-28 22:50:56 +0900

    project 생성

---
+++ Misik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Misik
+//
+//  Created by Haeseok Lee on 1/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MisikTests/MisikTests.swift
@@ -0,0 +1,17 @@
+//
+//  MisikTests.swift
+//  MisikTests
+//
+//  Created by Haeseok Lee on 1/28/25.
+//
+
+import Testing
+@testable import Misik
+
+struct MisikTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
